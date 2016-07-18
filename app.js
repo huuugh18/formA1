e437bb54cff6f7f213e061c127e0bafd1440c7d7
huuugh18 <hugh.oneill.4@gmail.com>
2016-07-18 08:08:09 -0600

    post sunday codeathon forms added html converted

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//-----------------NODES-------------------------
 var express = require('express');
 var mailer = require('express-mailer')
 var path = require('path');
@@ -5,6 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,20 +15,20 @@ const fit4mePW = process.env.password
 var app = express();
 
 
-// express mailer setup
-mailer.extend(app, {
-  from: fit4meMail,
-  host: 'smtp.gmail.com',
-  secureConnection: true,
-  port: 465,
-  transportMethod: 'SMTP',
-  auth: {
-    user:fit4meMail,
-    pass: fit4mePW
-  }
-})
+//------------ express mailer setup---------------------
+  mailer.extend(app, {
+    from: fit4meMail,
+    host: 'smtp.gmail.com',
+    secureConnection: true,
+    port: 465,
+    transportMethod: 'SMTP',
+    auth: {
+      user:fit4meMail,
+      pass: fit4mePW
+    }
+  })
 
-// view engine setup
+//-------------- view engine setup------------------
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -41,8 +43,58 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.get('/log', function(req,res){
-  res.render('formlog',{})
+function getFormFileName(programID){
+  return 'form'+programID
+}
+app.get('/programs', function(req,res){
+  fs.readFile('./data.json', 'utf8', function (err,data){
+    var list = JSON.parse(data)
+    console.log(list)
+    res.render('programlist',{programs:list})
+  })
+})
+app.get('/programs/new', function (req,res){
+  res.render('newprogram')
+})
+app.post('/updateProgram/:programID', function(req,res){
+  fs.readFile('./data.json', 'utf8', function (err,data){
+    var allPrograms = JSON.parse(data)
+    var program = allPrograms.find(function(p){
+      return p.id===req.params.programID
+    })
+    program.name=req.body.programName
+    fs.writeFile('./data.json', JSON.stringify(allPrograms), function(err) {
+      res.redirect('/programs')
+    })
+    
+  })
+})
+app.get('/programs/:programID', function (req,res){
+  fs.readFile('./data.json', 'utf8', function (err,data){
+    var allPrograms = JSON.parse(data)
+    var program = allPrograms.find(function(p){
+      return p.id===req.params.programID
+    })
+    res.render('editProgram',program)
+  })
+})
+app.post('/processNewProgram', function(req,res){
+  var newProg = {id:req.body.programID,name:req.body.programName}
+  fs.readFile('./data.json', 'utf8', function (err,data){
+    var allPrograms = JSON.parse(data)
+    allPrograms.push(newProg)
+    fs.writeFile('./data.json', JSON.stringify(allPrograms), function(err) {
+      res.redirect('/programs')
+    })
+    
+  })
+})
+  
+
+app.get('/log/:programID', function(req,res){
+  console.log(req.params.programID)
+  var form='form'+req.params.programID
+  res.render(form,{})
 })
 
 // original test functions
@@ -61,22 +113,6 @@ app.get('/sendmail', function(req,res){
   })
 })
 
-
-
-app.post('/processform', function(req,res){
-  console.log('I hear you')
-  app.mailer.send('email', {
-    to: req.body.emailer,
-    subject: 'Test Mailer from'+ req.body.name,
-  }, function (err){
-    if (err) {
-      console.log(err)
-      res.send('error sending email')
-      return;
-    }
-    res.send('Email Sent')
-  })
-})
 //======================================
 // Email Render Tests===================
 //======================================
@@ -117,6 +153,20 @@ app.get('/test1', function(req, res){
     w4d6AL: checkAerobic(undefined, 'hockey'),
     //-----------------Day 7 Rest--------------
    })
+})
+app.post('/processform', function(req,res){
+  console.log('I hear you')
+  app.mailer.send('email', {
+    to: req.body.emailer,
+    subject: 'Test Mailer from'+ req.body.name,
+  }, function (err){
+    if (err) {
+      console.log(err)
+      res.send('error sending email')
+      return;
+    }
+    res.send('Email Sent')
+  })
 })
 
 //========================================
